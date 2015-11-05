10b2334c54da944dee250cb266abbba6af4f07e9
sahamednyc <sahamed2000@gmail.com>
2015-11-04 23:04:25 -0500

    Update Account.java
    Make Account an abstract class which will be extended for Savings, Checking and Maxi Savings Account class.

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -1,73 +1,78 @@
 package com.abc;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-public class Account {
-
+public abstract class Account {
     public static final int CHECKING = 0;
     public static final int SAVINGS = 1;
     public static final int MAXI_SAVINGS = 2;
 
-    private final int accountType;
-    public List<Transaction> transactions;
-
-    public Account(int accountType) {
-        this.accountType = accountType;
-        this.transactions = new ArrayList<Transaction>();
-    }
-
-    public void deposit(double amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException("amount must be greater than zero");
+    public List<Transaction> transactions = new ArrayList<Transaction>();
+    
+    private ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    
+    public abstract int getAccountType();
+    public abstract BigDecimal interestEarned();
+    public abstract String statementForAccount();
+    
+    
+    public boolean deposit(BigDecimal amount) {
+    	boolean success = false;
+        if (amount.signum() < 0 || amount.compareTo(BigDecimal.ZERO) == 0) {
+            throw new IllegalArgumentException(HelperUtil.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
         } else {
-            transactions.add(new Transaction(amount));
+        	//Deposit Transaction need to be exclusive with Withdraw and sum Transactions calculation.
+        	readWriteLock.writeLock().lock();
+        	success = transactions.add(new Transaction(amount));
+        	readWriteLock.writeLock().unlock();
         }
+        return success;
+        
     }
-
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        transactions.add(new Transaction(-amount));
-    }
-}
-
-    public double interestEarned() {
-        double amount = sumTransactions();
-        switch(accountType){
-            case SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.001;
-                else
-                    return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
-            case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
-            default:
-                return amount * 0.001;
+    public boolean withdraw(BigDecimal amount) {
+    	boolean success = false;
+        if (amount.signum() < 0 || amount.compareTo(BigDecimal.ZERO) == 0) {
+            throw new IllegalArgumentException(HelperUtil.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
+        } else {
+        	//Withdraw Transaction need to be exclusive with Deposit and sum Transactions calculation.
+        	readWriteLock.writeLock().lock();
+        	success = transactions.add(new Transaction(amount.negate()));
+        	readWriteLock.writeLock().unlock();
         }
+        return success;
     }
-
-    public double sumTransactions() {
-       return checkIfTransactionsExist(true);
-    }
-
-    private double checkIfTransactionsExist(boolean checkAll) {
-        double amount = 0.0;
-        for (Transaction t: transactions)
-            amount += t.amount;
+    public BigDecimal sumTransactions() {
+    	readWriteLock.readLock().lock();
+        BigDecimal amount = BigDecimal.ZERO;
+        for (Transaction t: transactions){
+        	amount = amount.add(t.amount);
+        }   
+        readWriteLock.readLock().unlock();
         return amount;
     }
+    
+    public String transactionDetails(){
+    	StringBuffer s = new StringBuffer();
+       	readWriteLock.readLock().lock();
+        BigDecimal total = new BigDecimal(0.0);
+        for (Transaction t : transactions) {
+        	s.append("  ");
+        	s.append(t.amount.signum() < 0 ? HelperUtil.WITHDRAWAL : HelperUtil.DEPOSIT);
+        	s.append(" ");
+        	s.append(HelperUtil.toDollars(t.amount));
+        	s.append(HelperUtil.NEW_LINE);
+        	total = total.add(t.amount);
+        }
+        s.append(HelperUtil.TOTAL);
+        s.append(HelperUtil.toDollars(total));
+        readWriteLock.readLock().unlock();
 
-    public int getAccountType() {
-        return accountType;
+        return s.toString();
     }
-
+    
 }
+
