c756c959d4197d3dfbd6db47c16caee539e8ee35
sahamednyc <sahamed2000@gmail.com>
2015-11-04 23:31:19 -0500

    Update CustomerTest.java
    Modified to use new changes and Transfer method test.

--- src/test/java/com/abc/CustomerTest.java
+++ src/test/java/com/abc/CustomerTest.java
@@ -5,19 +5,22 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.math.BigDecimal;
+
 public class CustomerTest {
+	private static final double DOUBLE_DELTA = 1e-15;
 
     @Test //Test customer statement generation
     public void testApp(){
 
-        Account checkingAccount = new Account(Account.CHECKING);
-        Account savingsAccount = new Account(Account.SAVINGS);
+        Account checkingAccount = new CheckingAccount();
+        Account savingsAccount = new SavingsAccount();
 
         Customer henry = new Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount);
 
-        checkingAccount.deposit(100.0);
-        savingsAccount.deposit(4000.0);
-        savingsAccount.withdraw(200.0);
+        checkingAccount.deposit(new BigDecimal(100.0));
+        savingsAccount.deposit(new BigDecimal(4000.0));
+        savingsAccount.withdraw(new BigDecimal(200.0));
 
         assertEquals("Statement for Henry\n" +
                 "\n" +
@@ -35,23 +38,38 @@ public class CustomerTest {
 
     @Test
     public void testOneAccount(){
-        Customer oscar = new Customer("Oscar").openAccount(new Account(Account.SAVINGS));
+        Customer oscar = new Customer("Oscar").openAccount(new SavingsAccount());
         assertEquals(1, oscar.getNumberOfAccounts());
     }
 
     @Test
     public void testTwoAccount(){
         Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
+                .openAccount(new SavingsAccount());
+        oscar.openAccount(new CheckingAccount());
         assertEquals(2, oscar.getNumberOfAccounts());
     }
 
-    @Ignore
+    @Test
     public void testThreeAcounts() {
         Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
+                .openAccount(new SavingsAccount());
+        oscar.openAccount(new CheckingAccount());
+        oscar.openAccount(new MaxiSavingsAccount());
         assertEquals(3, oscar.getNumberOfAccounts());
+    }
+    
+    @Test
+    public void testTransfer(){
+    	Account checkingAccount = new CheckingAccount();
+    	Account savingsAccount = new SavingsAccount();
+    	
+    	Customer shameem = new Customer("Shameem").openAccount(checkingAccount);
+    	shameem.openAccount(savingsAccount);
+    	
+    	savingsAccount.deposit(HelperUtil.oneThousand);//1000 in savings
+    	BigDecimal fiveHundrad = new BigDecimal(500);
+    	shameem.transfer(savingsAccount, checkingAccount, fiveHundrad);
+    	assertEquals(fiveHundrad.doubleValue(),savingsAccount.sumTransactions().doubleValue(),DOUBLE_DELTA);
     }
 }
