f4c999e20d56b1b3b5a36bbc0077f47af582d262
sahamednyc <sahamed2000@gmail.com>
2015-11-04 23:06:22 -0500

    Create SavingsAccount.java
    This is Savings account class

---
+++ src/main/java/com/abc/SavingsAccount.java
@@ -0,0 +1,28 @@
+import java.math.BigDecimal;
+
+public class SavingsAccount extends Account{
+
+	@Override
+	public int getAccountType() {
+		return Account.SAVINGS;
+	}
+
+	@Override
+	public BigDecimal interestEarned() {
+		BigDecimal amount = sumTransactions();
+		if (amount.compareTo(HelperUtil.oneThousand) <= 0)
+            return amount.multiply(HelperUtil.pointOnePerCent);
+        else
+            return (amount.subtract(HelperUtil.oneThousand).multiply(HelperUtil.pointTwoPerCent)).add(BigDecimal.ONE);
+	}
+
+	@Override
+	public String statementForAccount() {
+		StringBuffer s = new StringBuffer("");
+        s.append(HelperUtil.SAVINGS_ACCOUNT);
+        s.append(HelperUtil.NEW_LINE);
+        s.append(transactionDetails());
+        return s.toString();
+	}
+
+}
