49f1fe66a25f0359b59cab9403ce562ad051a32e
sahamednyc <sahamed2000@gmail.com>
2015-11-04 23:05:31 -0500

    Create CheckingAccount.java
    This is my Checking Account class

---
+++ src/main/java/com/abc/CheckingAccount.java
@@ -0,0 +1,26 @@
+import java.math.BigDecimal;
+
+public class CheckingAccount extends Account{
+
+	@Override
+	public int getAccountType() {
+		return Account.CHECKING;
+	}
+
+	@Override
+	public BigDecimal interestEarned() {
+		BigDecimal amount = sumTransactions();
+		return amount.multiply(HelperUtil.pointOnePerCent);
+	}
+
+	@Override
+	public String statementForAccount() {
+		StringBuffer s = new StringBuffer("");
+        s.append(HelperUtil.CHECKING_ACCOUNT);
+        s.append(HelperUtil.NEW_LINE);
+        s.append(transactionDetails());
+        return s.toString();
+		
+	}
+
+}
