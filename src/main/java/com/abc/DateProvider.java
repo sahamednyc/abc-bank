19d1d972b5a780b36f9bb088cdd4c1bbbb9773e6
sahamednyc <sahamed2000@gmail.com>
2015-11-04 23:11:09 -0500

    Update DateProvider.java
    Using Enum for Singleton Class.

--- src/main/java/com/abc/DateProvider.java
+++ src/main/java/com/abc/DateProvider.java
@@ -3,12 +3,10 @@ package com.abc;
 import java.util.Calendar;
 import java.util.Date;
 
-public class DateProvider {
-    private static DateProvider instance = null;
+public enum DateProvider {
+    instance;
 
     public static DateProvider getInstance() {
-        if (instance == null)
-            instance = new DateProvider();
         return instance;
     }
 
