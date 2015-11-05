1c573f630b379d1c680308a1ed417857c03a65db
sahamednyc <sahamed2000@gmail.com>
2015-11-04 23:12:44 -0500

    Update Bank.java
    Modifying Bank to improve codes.

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -1,5 +1,6 @@
 package com.abc;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,25 +16,29 @@ public class Bank {
     }
 
     public String customerSummary() {
-        String summary = "Customer Summary";
-        for (Customer c : customers)
-            summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
-        return summary;
+        StringBuffer summary = new StringBuffer(HelperUtil.CUSTOMER_SUMMARY);
+        for (Customer c : customers){
+        	summary.append(HelperUtil.NEW_LINE);
+        	summary.append(c.getName());
+        	summary.append(" (");
+        	summary.append(HelperUtil.format(c.getNumberOfAccounts(), "account"));
+        	summary.append(")");
+        }	
+        return summary.toString();
     }
 
-    //Make sure correct plural of word is created based on the number passed in:
-    //If number passed in is 1 just return the word otherwise add an 's' at the end
-    private String format(int number, String word) {
-        return number + " " + (number == 1 ? word : word + "s");
-    }
+    
 
-    public double totalInterestPaid() {
-        double total = 0;
+    public BigDecimal totalInterestPaid() {
+    	BigDecimal total = BigDecimal.ZERO;
         for(Customer c: customers)
-            total += c.totalInterestEarned();
+            total = total.add(c.totalInterestEarned());
         return total;
     }
-
+    
+ 
+//What is the purpose?
+    /*
     public String getFirstCustomer() {
         try {
             customers = null;
@@ -43,4 +48,5 @@ public class Bank {
             return "Error";
         }
     }
+    */
 }
